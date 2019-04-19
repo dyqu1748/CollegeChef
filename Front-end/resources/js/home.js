80d5ae82dc125e34ab7fee9547cfc1f5a6c8c142
dyqu1748 <47007591+dyqu1748@users.noreply.github.com>
2019-04-18 19:26:34 -0600

    Fixed cards; Added food pref filter; Tags JS

--- Front-end/resources/js/home.js
+++ Front-end/resources/js/home.js
@@ -40,12 +40,12 @@ function restrictions(temp)
         document.getElementById('gluten').innerHTML = "";
         if(temp.vegetarian == true)
         {
-            document.getElementById('vegitarian').innerHTML += '<p> Vegitarian: Yes</p>';
+            document.getElementById('vegitarian').innerHTML += '<p> Vegetarian: Yes</p>';
     
         }
         else if(temp.vegetarian == false)
         {
-            document.getElementById('vegitarian').innerHTML += '<p> Vegitarian: No</p>';
+            document.getElementById('vegitarian').innerHTML += '<p> Vegetarian: No</p>';
         }
         if(temp.vegan == true)
         {
@@ -110,12 +110,125 @@ function restrictions(temp)
         xhttp.setRequestHeader("X-RapidAPI-Key", "601fdf014cmsh9774814f1ee4e3dp10ecadjsn1de8cb4425cc");
         xhttp.send();
     }
+
+    //////Working////////////////////////////////////////////////////////////////////////////////////
+
+    function stuff(element) 
+    {   
+        // if(card == undefined)
+        // {
+        //     card = "";
+        // }
+        console.log("TEST1")
+        var xhttp1 = new XMLHttpRequest();
+        var id= element.id;
+        // console.log("1");
+        url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/"+ id+ "/information"
+        
+        var id;
+        // card += '<div class="container-fluid">';
+        // card +=  '<div class="row">';
+        // card += '<div class="card-deck">';
+        // console.log("1");
+        xhttp1.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // console.log("1");
+            //console.log(this.response)
+            var json = JSON.parse(this.response);            
+            console.log("JSON: ", json);
+            var cardStyle;
+            if(json.instructions == null)
+            {   
+                // console.log("NO");
+                // cardStyle = 'none';
+                // generateCards(element, cardStyle);
+            }
+            else
+            {
+                cardStyle = 'visible';
+                generateCards(element, cardStyle);
+            }           
+        }
+        else {
+            console.log("Something went wrong")
+        }
+        }
+        xhttp1.open("GET", url,true);//, true);
+        xhttp1.setRequestHeader("X-RapidAPI-Key", "601fdf014cmsh9774814f1ee4e3dp10ecadjsn1de8cb4425cc");
+        xhttp1.send();
+    }
+
+
+    function generateCards(response, style)
+    {
+        var cardStyle = style;
+        var card = "";
+        if(cardStyle == 'visible')
+        {
+            console.log("CARD TEST1: ", card);
+            card += '<div class="card" style="width: 13rem" align="center"><div class="card__info-hover">';
+            card += '<div class="card__ingred-info"><span class="card__ingred"> Matching Ingredients: ' +  response['usedIngredientCount'] + '</span>';
+            card += '<span class="card__ingred"> Missing Ingredients: ' +  response['missedIngredientCount'] + '</span></div></div>';
+            card +=  '<div class="card__img" style="background-image: url(\''+ response['image'] +'\')"></div>';
+            card += '<a href="#" id='+ response.id + ' class="card_link" data-toggle="modal" data-target="#exampleModal" onclick="displayMoreInfo(this)">';
+            card += '<div class="card__img--hover" style="background-image: url(\''+ response['image'] +'\')">';
+            card += '</div></a>';
+            card += '<a href="#" id='+ response.id + ' class="card_link" data-toggle="modal" data-target="#exampleModal" onclick="displayMoreInfo(this)">';
+            card +=  '<div class="card__info"><h4 class="card__title">';
+            // card +=  '<img class="card-img-top src='+ response[i]['image'];
+            // card +=  '<h5 class="card-title">'; 
+            card +=  response['title']; 
+            card +=  '</h4></div></a></div>';
+            console.log("CARD TEST2: ", card);  
+            document.getElementById("recipes-location").innerHTML += card;   
+            if(card == "undefined")
+            {
+                card = "";
+            }
+            
+            // }
+            // card+='</div></div></div>';
+
+        }
+    }
+
+
+
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     function loadDoc() 
     {
         var xhttp = new XMLHttpRequest();
         var all_ingredients = document.getElementsByName('ingredients');
+        var all_restrictions = document.getElementsByName('filter');
         var ingredients = "";
+        var restrictions = "";
         var counter = 0;
+
+        var ingTag = $('#tags').tagsinput('items');
+
+        for(var i = 0; i <all_ingredients.length; i++)
+        {
+            
+          if(all_ingredients[i].selected == true)
+          {
+            // console.log("i: ", all_ingredients[i].value);
+            counter++;
+            ingredients += all_ingredients[i].value + '%2C'
+          }
+        }
+        for(var i = 0; i < ingTag.length; i++)
+        {
+            // console.log("j: ", ingTag[i]);
+            counter++;
+            ingredients += ingTag[i] + '%2C'
+        }
+        for(var i = 0; i < all_restrictions.length; i++)
+        {
+            restrictions += all_restrictions[i].value 
+        }
         for(var i = 0; i <all_ingredients.length; i++)
         {
           if(all_ingredients[i].selected == true)
@@ -124,12 +237,7 @@ function restrictions(temp)
             ingredients += all_ingredients[i].value + '%2C'
           }
         }
-        //document.getElementById('ingredients');  // assume it returns this array ['sugar, 'spice', 'everything nice']
-      //  ing_url = "";
-        //ingredients.forEach(function(item)
-        //{
-        //ing_url += item + "%2C"
-        //})
+
         if (counter == 0)
         {
             alert("Please select at least one ingredient to begin searching for recipes.");
@@ -138,63 +246,54 @@ function restrictions(temp)
         ingredients = ingredients.slice(0,-3);
         console.log(ingredients);
         // There will be a , in the end make sure you remove it
-        base_url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=2&ignorePantry=false&ingredients=";
+        base_url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=10&ranking=2&ignorePantry=false&ingredients=";
         url = base_url + ingredients;
         console.log(`url is : ${url}`);
         xhttp.onreadystatechange = function() 
         {
             if (this.readyState == 4 && this.status == 200) 
             {
-                var card = "";
-                card += '<div class="container-fluid">';
-                card +=  '<div class="row">';
-                card += '<div class="card-deck">';
-                response = JSON.parse(this.response);
+ 
+                response = JSON.parse(this.response); 
                 for(var i = 0; i < response.length; i++)
                 {
-                    //var card =  "";
-                    // card += '<div class="row">';
-                    // card += '<div class="col-sm-6">';
-                    card += '<article class="card"><div class="card__info-hover"></div>';
-                    card +=  '<div class="card__img" style="background-image: url(\''+ response[i]['image'] +'\')"></div>';
-                    card += '<a href="#" id='+ response[i].id + ' class="card_link" data-toggle="modal" data-target="#exampleModal" onclick="displayMoreInfo(this)">';
-                    card += '<div class="card__img--hover" style="background-image: url(\''+ response[i]['image'] +'\')">';
-                    card+= '</div></a>';
-                    card +=  '<div class="card__info"><h4 class="card__title">';
-                    // card +=  '<img class="card-img-top src='+ response[i]['image'];
-                    // card +=  '<h5 class="card-title">'; 
-                    card +=  response[i]['title']; 
-                    card +=  '</h4></div></article>';
-                    // card +=  response[i]['usedIngredientCount'];
-                    // card +=  '<br> Missed Ingredient Count: ';
-                    // card +=  response[i]['missedIngredientCount'];
-                    // card +=  '</p></div></article>';
-                    // card += '<btn id='+ response[i].id';
-                    // card +=  ' href="#" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" onclick="displayMoreInfo(this)">See More Information</btn></div></div>';    
-                    // card += '</div></div>';
-                    // card += '<article class="card"><div class="card__info-hover"></div><div class="card__img" style="background-image: url(\''+response[i]['image'] +'\')"></div><a href="#" class="card_link"><div class="card__img--hover" style="background-image: url(\''+response[i]['image'] +'\')"></div></a><div class="card__info"><h3 class="card__title">Crisp Spanish tortilla Matzo brei</h3></div></article>';
+                
+                    stuff(response[i]);
+                    // card += '<article class="card" style="width: 12rem"><div class="card__info-hover">';
+                    // card += '<div class="card__ingred-info"><span class="card__ingred"> Matching Ingredients: ' +  response[i]['usedIngredientCount'] + '</span>';
+                    // card += '<span class="card__ingred"> Missing Ingredients: ' +  response[i]['missedIngredientCount'] + '</span></div></div>';
+                    // card +=  '<div class="card__img" style="background-image: url(\''+ response[i]['image'] +'\')"></div>';
+                    // card += '<a href="#" id='+ response[i].id + ' class="card_link" data-toggle="modal" data-target="#exampleModal" onclick="displayMoreInfo(this)">';
+                    // card += '<div class="card__img--hover" style="background-image: url(\''+ response[i]['image'] +'\')">';
+                    // card += '</div></a>';
+                    // card += '<a href="#" id='+ response[i].id + ' class="card_link" data-toggle="modal" data-target="#exampleModal" onclick="displayMoreInfo(this)">';
+                    // card +=  '<div class="card__info"><h4 class="card__title">';
+                    // // card +=  '<img class="card-img-top src='+ response[i]['image'];
+                    // // card +=  '<h5 class="card-title">'; 
+                    // card +=  response[i]['title']; 
+                    // card +=  '</h4></div></a></article>';
+                    
                    
                     
                 }
-                card+='</div></div></div>';
-             	document.getElementById("recipes-location").innerHTML = card;   
-             	console.log(card);
             }
-        };
+        }
         
         xhttp.open("GET", url, true);
         xhttp.setRequestHeader("X-RapidAPI-Key", "601fdf014cmsh9774814f1ee4e3dp10ecadjsn1de8cb4425cc");
         xhttp.send();
         $(".loadRecp").fadeIn();
         setTimeout(showRecipes, 2000);
-    };
+    }
 
+    
     function showRecipes()
     {
         $("#section-a").fadeOut();
         $(".loadRecp").fadeOut();
         $("#section-b").fadeIn();
         window.location.hash = "section-b";
+        document.getElementById("client").value = "";
     }
 
     function newsearch()
